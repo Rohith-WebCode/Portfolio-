ab2615692337beed2d13f9a838eac2ee97f1573b
Rohith Prakash <155292422+Rohith-WebCode@users.noreply.github.com>
2024-10-29 13:09:04 +0530

    Update Banner.js

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 const Banner = () => {
   const navigate = useNavigate();
   const handleClick = ()=>{
-    navigate('/contact us')
+    navigate('/contact')
   }
   return (
     <div className='Banner'>
